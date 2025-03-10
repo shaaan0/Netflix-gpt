9894041ec45586c19ba732c153afe6528dcf0d4c
Sudipta Singh <ss.dps47@gmail.com>
2025-03-10 18:36:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+
+function App(){
+    return(
+
+        <div className="text-5xl" >Namaste</div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,  // Ensure Webpack processes .js and .jsx files
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader","postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    hot: true,
+    port: 3000,
+  },
+  mode: "development",
+};
+
